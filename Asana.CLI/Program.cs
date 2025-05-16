6a2647551ab5559cc243a3b153df96e9f5851028
Aacro <148156489+Aacro@users.noreply.github.com>
2025-05-16 10:52:03 -0400

    First C sharp project

---
+++ Asana.CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Asana.Library.Models;
+
+ToDo MyFirstToDo = new ToDo
+{
+    Name = "Buy groceries",
+    Description = "Milk, Bread, Eggs",
+    DueDate = DateTime.Now.AddDays(2),
+    IsDone = false,
+    Priority = "High"
+};
---
+++ Asana.Library/Models/ToDo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Asana.Library.Models
+{
+    public class ToDo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsDone { get; set; }
+        public string Priority { get; set; }
+        
+    }
+}
