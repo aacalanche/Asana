d0f4e40c66275afac53c86cf32b67dba85b74f78
Aacro <148156489+Aacro@users.noreply.github.com>
2025-05-19 17:57:31 -0400

    changed IsDone for IsCompleted

--- Asana.Library/Models/ToDo.cs
+++ Asana.Library/Models/ToDo.cs
@@ -6,8 +6,7 @@ namespace Asana.Library.Models
     {
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public DateTime? DueDate { get; set; }
-        public bool? IsDone { get; set; }
+        public bool? IsCompleted { get; set; }
         public string? Priority { get; set; }
         
         public override string ToString()
