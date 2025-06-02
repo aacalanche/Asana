7337b17cf3c47fc58e8d0259596423d34bd7432b
Aacro <148156489+Aacro@users.noreply.github.com>
2025-06-02 10:54:19 -0400

    Add raw assets and styles for the application
    - Created AboutAssets.txt for raw asset deployment instructions.
    - Added splash.svg for the application splash screen.
    - Introduced Colors.xaml to define the color palette for the application.
    - Implemented Styles.xaml to establish consistent styling for various UI components including buttons, labels, and pages.

--- Asana.Library/obj/Debug/net9.0/Asana.Library.AssemblyInfo.cs
+++ Asana.Library/obj/Debug/net9.0/Asana.Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asana.Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094378466b8e6b261578645e1adf517cc9daeaa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4152a0eafb9197f96f0bb3a9c97d98622a6ab5c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asana.Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asana.Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
