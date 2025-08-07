8eff0d48c256822ac0f5949aae4bd5c49dadbc55
Aacro <148156489+Aacro@users.noreply.github.com>
2025-08-06 23:55:30 -0500

    cache again

--- Asana.Library/obj/Debug/net9.0/Asana.Library.AssemblyInfo.cs
+++ Asana.Library/obj/Debug/net9.0/Asana.Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asana.Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a62d1b97d1fddfc78f7de7e08ae34132666eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a6412c8d955524556315f2ad563fdc50696bd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asana.Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asana.Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
